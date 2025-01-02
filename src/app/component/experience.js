e96ad9638e2a6d9bf63e422e246d3e6e927ce76e
harshit340 <harshitshrivastava340@gmail.com>
2025-01-02 15:22:56 +0530

    working on responsivness

---
+++ src/app/component/experience.js
@@ -0,0 +1,72 @@
+import codifyNowImg from '../image/image.png';
+import hackerHouseImg from '../image/WhatsApp Image 2025-01-02 at 01.21.28_d68a06c5.jpg';
+import rideHackImg from '../image/RideHack.jpg';
+import hackCBSImg from '../image/HackCBS.jpg';
+
+export const workExperienceData = [
+  {
+    title: "Full-Stack Developer - (Internship)",
+    company: "CodifyNow",
+    date: "Oct 2024 - Dec 2024",
+    description: "As a Full-Stack Developer, I led the development of an anonymous chatting platform, contributing to both frontend and backend functionalities to ensure a seamless user experience. Additionally, I designed and built two promotional contact websites, focusing on user-centric design and performance optimization.",
+    image: codifyNowImg, // Imported image
+  },
+  {
+    title: "Hacker House Goa",
+    company: "WEB3 Hackathon",
+    date: "Aug 2024",
+    description: "Contributed to blockchain-based development and innovative solutions during the WEB3 Hackathon, focusing on user-centric decentralized applications.",
+    image: hackerHouseImg,
+  },
+  {
+    title: "RideHack Phase-2",
+    company: "Startup Hackathon",
+    date: "Sep 2024",
+    description: "Participated in the Startup Hackathon, delivering scalable digital solutions with a focus on innovation and teamwork.",
+    image: rideHackImg,
+  },
+  {
+    title: "HackCBS 7.0",
+    company: "Hackathon",
+    date: "Nov 2024",
+    description: "Developed a full-stack project, focusing on collaborative problem-solving and leveraging modern web technologies.",
+    image: hackCBSImg,
+  },
+];
+
+export const projects = [
+    {
+      title: "NextPay",
+      description: "NextPay is engineered with cutting-edge blockchain technology, ensuring that your Ethereum can move freely across various EVM-based blockchains without the usual hassle.",
+      stack: "TypeScript",
+      link: "https://next-pay-eta.vercel.app/",
+    },
+    {
+      title: "CraveFeed",
+      description: "Cravefeed, the ultimate social media platform designed exclusively for foodies and culinary enthusiasts.",
+      stack: "TypeScript",
+      link: "#",
+    },
+    {
+      title: "Strangrly",
+      description: "An anonymous chatting platform designed to connect users in a secure and private environment. ",
+      stack: "TypeScript",
+      link: "https://github.com/harshit340/live_chats",
+    },
+    {
+      title: "Foody",
+      description: "A dynamic food ordering platform designed to simplify the online dining experience. I developed a user-friendly interface, enabling seamless navigation through menus and an efficient ordering process.",
+      stack: "JavaScript",
+      link: "https://github.com/harshit340/Foody",
+    },
+    {
+      title: "MeetingSchedular",
+      description: "A sleek and responsive interface for scheduling meetings, built using Next.js",
+      stack: "JavaScript",
+      link: "https://github.com/harshit340/Foody",
+    },
+    // Add more projects as needed
+  ];
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,126 @@
 import { Button, Card } from 'antd';
+import "./style.css"
+import {workExperienceData} from './component/experience';
+import { projects } from './component/experience';
+import Image from 'next/image';
+
+
 
 
 export default function Home() {
   return (
-    <div style={{ display: "flex" , justifyContent:"center" }}>
-      <div>
-        <Card style={{ width: 400, border: "0.5px solid grey" }}>
-          <div>image</div>
-          <h1>Harshit Shrivastava</h1>
-          <h2>Software Developer</h2>
-          <h5>Full-Stack Developer dedicated to creating scalable, user-focused web applications that drive innovation and growth. Combining expertise in front-end design, back-end systems, and modern frameworks, I craft digital experiences that align with business goals and deliver measurable impact.</h5>
-          <Button type='primary'>Contact Me</Button>
-          <div>----------------------</div>
-          <div>linkedin</div>
-          <div>Github</div>
-          <div>leetcode</div>
-        </Card></div>
-      <div>
+    <div className='main-align' >
 
+      <Card className='aside-info' style={{
+        width: "400px",
+        padding: "1.5rem",
+
+        boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
+      }}>
+        <Image src="/my.jpg" alt='heloo' width={150} height={150} className="profile-image"></Image>
+        <h1 style={{ fontSize: "20px", fontWeight: "bold" , marginTop:"20px"}}>Harshit Shrivastava</h1>
+        <h2 style={{ marginTop: "-2%", fontWeight: 400, color: "gray" }}>Software Developer</h2>
+        <h5 style={{ fontSize: "15px", paddingTop: "10px", color: "rgb(74, 70, 70)" }}>Full-Stack Developer focused on building scalable, user-centric web applications that drive innovation and deliver measurable impact. With expertise in front-end design, back-end systems, and modern frameworks, I create digital solutions aligned with business goals</h5>
+        <Button type='primary' style={{ width: "100%", margin: "1rem 0", marginTop: "2rem" }}>Contact Me</Button>
+        <div>----------------------</div>
+        <div>linkedin</div>
+        <div>Github</div>
+        <div>leetcode</div>
         <div>
-          <Card style={{ width: 600, height: 300 }} >
-            <h1>About me</h1>
-            <p>Full-Stack Developer passionate about building intuitive, scalable digital solutions that captivate users and drive measurable growth. As the <span>Co-founder</span>  and <span>Frontend Developer </span> at <span>CRAVEFEED</span>, an innovative app for food enthusiasts, With a strong foundation in modern web technologies and a deep commitment to mastering (DSA), I excel at solving complex problems efficiently, ensuring robust and optimized solutions. I strive to deliver user-centric designs that help brands stand out in an ever-evolving digital landscape.</p>
-            
-          </Card>
+          <p>Skills</p>
           <div>
-            <h1>Feature Project</h1>
             
           </div>
         </div>
+      </Card>
 
-        <div>
-          <div>Work Experience</div>
-          <Card>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
+      <div>
+
+        <div style={{ flex: 1, maxWidth: "800px" }}>
+          <Card className='About-info' style={{
+            padding: "1.5rem",
+            marginBottom: "1.5rem",
+            boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
+          }}>
+            <h1 style={{ fontSize: "26px", fontWeight: "bolder" }}>About me</h1>
+            <p style={{ fontSize: "16px", color: "rgb(74, 70, 70)" }}>Full-Stack Developer passionate about building intuitive, scalable digital solutions that captivate users and drive measurable growth. As the <span style={{ fontWeight: "bold" }}>Co-founder</span>  and <span style={{ fontWeight: "bold" }}>Frontend Developer </span> at <span style={{ fontWeight: "bold" }}>CRAVEFEED</span>, an innovative app for food enthusiasts, With a strong foundation in modern web technologies and a deep commitment to mastering (DSA), I excel at solving complex problems efficiently, ensuring robust and optimized solutions. I strive to deliver user-centric designs that help brands stand out in an ever-evolving digital landscape.</p>
+
           </Card>
+          <div className='feature-info'>
+            <h1 style={{ fontSize: "18px", fontWeight: "bolder" }}>Featured Projects</h1>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+      {projects.map((project, index) => (
+        <div
+          key={index}
+          className="border rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow"
+          style={{display:"flex" , flexDirection:"column" , justifyContent:"space-between"}}
+        > <div>
+          <h3 className="text-lg font-semibold">{project.title}</h3>
+          <p className="text-sm text-gray-600">{project.description}</p>
+          </div>
+          <div style={{display:"flex" , justifyContent:"space-between" , alignItems:"center"}}>
+          <p className="text-sm font-medium text-blue-600">{project.stack}</p>
+          <a
+            href={project.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-500 hover:underline mt-2 inline-block"
+          >
+            View Project →
+          </a></div>
+        </div>
+      ))}
+    </div>
+          </div>
         </div>
 
+      
+<div className="experience-info">
+          <div style={{ fontSize: "18px", fontWeight: "bolder" }}>Work Experience</div>
+          
+            <Card
+              
+              style={{
+                padding: "1.5rem",
+                marginBottom: "1.5rem",
+                boxShadow: "0 4px 6px rgba(0,0,0,0.1)",
+                maxWidth: "800px",
+                marginTop:"20px"
+              }}
+            > 
+            <div>
+            {workExperienceData.map((experience, index) => (
+              <div key={index}>
+                <div style={{display:"flex" }}>
+                <Image src={experience.image} width={65} height={40} alt={experience.title} ></Image>
+                <div style={{marginLeft:"15px"}}>
+                <h1 style={{ fontSize: "16px", fontWeight: 700 }}>{experience.title}</h1>
+                <h5 style={{ fontSize: "14px", fontWeight: 500 }}>{experience.company}</h5>
+                <p style={{fontSize:"12px" , fontWeight:500}}>{experience.date}</p>
+                </div>
+                </div>
+                <div>
+                  
+                  <p
+                    style={{
+                      fontSize: "16px",
+                      color: "rgb(74, 70, 70)",
+                      marginTop: "5px",
+                      marginBottom: "24px",
+                    }}
+                  >
+                    {experience.description}
+                  </p>
+                </div>
+              </div> ))}
+            </div>
+             
+            </Card>
+         
+        </div>
+      </div>
       </div>
 
-    </div>
+    
   );
 }
